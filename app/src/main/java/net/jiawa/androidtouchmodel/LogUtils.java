22bb75f22cfe1bf5ff53e431250926fbe2bcd8b4
xixia <xixia@jiawa.net>
2017-05-02 23:01:28 +0800

    [xixia][20170502][ 1.Log美化 ]

--- app/src/main/java/net/jiawa/androidtouchmodel/LogUtils.java
+++ app/src/main/java/net/jiawa/androidtouchmodel/LogUtils.java
@@ -3,6 +3,8 @@ package net.jiawa.androidtouchmodel;
 import android.util.Log;
 import android.view.MotionEvent;
 
+import java.util.Locale;
+
 public class LogUtils {
 
 	final static String TAG = "xixia-1";
@@ -25,12 +27,46 @@ public class LogUtils {
 		return motionEvent;
 	}
 
+	static String format(String str, int length) {
+		return String.format(Locale.ENGLISH, "%" + length + "s", str);
+	}
+
+	static int getMaxLength(String str1, String str2, String str3) {
+		return Math.max(Math.max(str1.length(), str2.length()), str3.length());
+	}
+
+	static int getClassLength() {
+		return Math.max(getMaxLength("MainActivity", "FirstFrameLayout", "SecondRelativeLayout"), "ThirdTextView".length());
+	}
+
+	static int getMethodLength() {
+		return getMaxLength("dispatchTouchEvent", "onInterceptTouchEvent", "onTouchEvent");
+	}
+
+	static int getEventLength() {
+		return getMaxLength("DOWN", "MOVE", "UP");
+	}
+
 	public static void printLog(Class clazz,String method, MotionEvent ev){
-		Log.d(TAG, clazz.getSimpleName() + ", " + method + ", " + getEvent(ev));
+		Log.d(TAG,
+				format(clazz.getSimpleName(), getClassLength())
+					   + ", " +
+				format(method, getMethodLength())
+					   + ", " +
+				format(getEvent(ev), getEventLength())
+			 );
 	}
 
 	public static void printLog(Class clazz,String method, MotionEvent ev, boolean eat){
-		Log.d(TAG, clazz.getSimpleName() + ", " + method + ", " + getEvent(ev) + ", EAT: " + eat);
+		Log.d(TAG,
+				format(clazz.getSimpleName(), getClassLength())
+						+ ", " +
+						format(method, getMethodLength())
+						+ ", " +
+						format(getEvent(ev), getEventLength())
+						+ ", " +
+						"EAT: " + eat
+		);
 	}
 
 	public static void printEventLog(Class mClass,String position,boolean isDispatch, MotionEvent event){
